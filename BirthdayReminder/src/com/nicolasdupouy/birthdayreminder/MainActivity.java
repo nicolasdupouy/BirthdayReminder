bf0a388de00835239080272305d62fd38a8f82f9
Nicolas Dupouy <nicolasdupouy@gmail.com>
2013-11-03 19:04:46 +0100

    Birthday reminder initial commit

---
+++ BirthdayReminder/src/com/nicolasdupouy/birthdayreminder/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nicolasdupouy.birthdayreminder;
+
+import com.example.birthdayreminder.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
