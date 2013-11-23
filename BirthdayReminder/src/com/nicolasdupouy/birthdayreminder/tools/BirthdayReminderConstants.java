8e830f098066f824fafe43125735d3bfde5479c6
Nicolas Dupouy <nicolasdupouy@gmail.com>
2013-11-23 19:51:38 +0100

    Create constants file.

---
+++ BirthdayReminder/src/com/nicolasdupouy/birthdayreminder/tools/BirthdayReminderConstants.java
@@ -0,0 +1,8 @@
+package com.nicolasdupouy.birthdayreminder.tools;
+
+public final class BirthdayReminderConstants {
+
+	private BirthdayReminderConstants() {}
+	
+	public static final String ENTRIES_STORAGE_FILENAME = "FichierPrenom.txt";
+}
